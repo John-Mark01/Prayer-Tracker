090d6b83993f84624c78687b06c8e77ca61be2d2
John-Mark Iliev <147177515+John-Mark01@users.noreply.github.com>
2025-10-27 22:02:12 +0200

    Initial Commit

---
+++ Prayer Tracker/Prayer_TrackerApp.swift
@@ -0,0 +1,32 @@
+//
+//  Prayer_TrackerApp.swift
+//  Prayer Tracker
+//
+//  Created by John-Mark Iliev on 27.10.25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Prayer_TrackerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
